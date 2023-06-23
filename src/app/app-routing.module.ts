817e1ec2915d6518e1459d47b81a8f29998e94b1
joaovictorpeix <joaovictor.dias1203@gmail.com>
2023-06-23 09:58:28 -0300

    Alteracoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PrincipalComponent } from './Principal/principal.component';
 import { ProdutoComponent } from './Produto/produto.component';
+import { CadastroComponent } from './Cadastro/cadastro.component';
+import { LoginComponent } from './Login/login.component';
 
 const routes: Routes = [
   { path: '', component: PrincipalComponent },
-  { path: 'produto', component: ProdutoComponent }
+  { path: 'produto', component: ProdutoComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'login', component: LoginComponent }
+
 ];
 
 @NgModule({
