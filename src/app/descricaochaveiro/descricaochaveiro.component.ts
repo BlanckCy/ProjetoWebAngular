c79dc2337699de4031d95b34ded84bbfd7f139b9
Santos Tamiris <tamiris.santos@scania.com>
2023-06-23 15:21:21 -0300

    update

---
+++ src/app/descricaochaveiro/descricaochaveiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descricaochaveiro',
+  templateUrl: './descricaochaveiro.component.html',
+  styleUrls: ['./descricaochaveiro.component.css']
+})
+export class DescricaochaveiroComponent {
+
+}
---
+++ src/app/descricaofunko/descricaofunko.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descricaofunko',
+  templateUrl: './descricaofunko.component.html',
+  styleUrls: ['./descricaofunko.component.css']
+})
+export class descricaofunkoComponent {
+
+}
